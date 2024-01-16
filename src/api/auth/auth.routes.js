e5c97362054ab9e9b4d79ffd83a71c7965fb8d54
eggyyuliandika <eggyyuliandika@gmail.com>
2024-01-16 12:05:38 +0800

    add router to login user

--- src/api/auth/auth.routes.js
+++ src/api/auth/auth.routes.js
@@ -48,5 +48,44 @@ router.post('/register', async (req, res, next) => {
   }
 });
 
+router.post('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400);
+      throw new Error('You must provide an email and a password.');
+    }
+
+    const existingUser = await findUserByEmail(email);
+
+    if (!existingUser) {
+      res.status(403);
+      throw new Error('Invalid login credentials.');
+    }
+
+    const validPassword = await bcrypt.compare(password, existingUser.password);
+    if (!validPassword) {
+      res.status(403);
+      throw new Error('Invalid login credentials.');
+    }
+
+    const jti = uuidv4();
+    const { accessToken, refreshToken } = generateTokens(existingUser, jti);
+    await addRefreshTokenToWhitelist({
+      jti,
+      refreshToken,
+      userId: existingUser.id,
+    });
+
+    res.json({
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+
 
 module.exports = router;
