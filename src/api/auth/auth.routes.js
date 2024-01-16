58f9608989c436e8a61d2fc2265b33ebe8db4fd2
eggyyuliandika <eggyyuliandika@gmail.com>
2024-01-16 12:04:36 +0800

    add router to register

---
+++ src/api/auth/auth.routes.js
@@ -0,0 +1,52 @@
+/* eslint-disable linebreak-style */
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
+const {
+  findUserByEmail,
+  createUserByEmailAndPassword,
+  findUserById,
+} = require('../users/users.services');
+const { generateTokens } = require('../../utils/jwt');
+const {
+  addRefreshTokenToWhitelist,
+  findRefreshTokenById,
+  deleteRefreshToken,
+  revokeTokens,
+} = require('./auth.services');
+const { hashToken } = require('../../utils/hashToken');
+
+const router = express.Router();
+
+router.post('/register', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400);
+      throw new Error('You must provide an email and a password.');
+    }
+
+    const existingUser = await findUserByEmail(email);
+
+    if (existingUser) {
+      res.status(400);
+      throw new Error('Email already in use.');
+    }
+
+    const user = await createUserByEmailAndPassword({ email, password });
+    const jti = uuidv4();
+    const { accessToken, refreshToken } = generateTokens(user, jti);
+    await addRefreshTokenToWhitelist({ jti, refreshToken, userId: user.id });
+
+    res.json({
+      accessToken,
+      refreshToken,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+module.exports = router;
